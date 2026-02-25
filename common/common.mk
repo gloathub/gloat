88743687ce98a7fc628d8cedc8250dcb59e83144
Ingy döt Net <ingy@ingy.net>
2026-02-25 13:32:04 -0800

    Fix for arm linker bug on macOS with shared libs

--- common/common.mk
+++ common/common.mk
@@ -1,2 +1,7 @@
 GLOJURE-VERSION := 0.6.5-rc6
 GLOJURE-REPO := https://github.com/gloathub/glojure
+
+# Go 1.26 has a linker bug on macOS arm64 for shared library builds
+ifdef IS-MACOS
+GO-VERSION := 1.25.7
+endif

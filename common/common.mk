0b136165748569592db49a57753174b7d03b0647
Ingy döt Net <ingy@ingy.net>
2026-05-06 18:33:36 -0400

    Version 0.1.27

--- common/common.mk
+++ common/common.mk
@@ -1,4 +1,4 @@
-GLOJURE-VERSION := 0.6.5-rc17
+GLOJURE-VERSION := 0.6.5-rc18
 GLOJURE-REPO := https://github.com/gloathub/glojure
 
 # Go 1.26 has a linker bug on macOS arm64 for shared library builds

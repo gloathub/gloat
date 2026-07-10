bb9cd15ad1bff970ab17d22256e030952760c5a9
Ingy döt Net <ingy@ingy.net>
2026-07-10 13:36:53 -0400

    Version 0.1.57

--- common/common.mk
+++ common/common.mk
@@ -1,6 +1,6 @@
-GLOAT-VERSION := 0.1.56
+GLOAT-VERSION := 0.1.57
 
-GLOJURE-VERSION := 0.6.7
+GLOJURE-VERSION := 0.6.8
 GLOJURE-REPO := https://github.com/glojurelang/glojure
 
 YAMLSCRIPT-VERSION := 0.2.8
